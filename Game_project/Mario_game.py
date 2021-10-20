547dc3cc455dde53acac081ff01777e20432da9b
semin1012 <semin1012@gmail.com>
2021-10-20 18:12:18 +0900

    Project #2 오브젝트 배치
    오브젝트 배치 및 리소스 이미지 수정

--- Game_project/Mario_game.py
+++ Game_project/Mario_game.py
@@ -4,7 +4,7 @@ import random
 # Game object class here
 class BackGround:
     def __init__(self):
-        self.image = load_image('mario_background1.png')
+        self.image = load_image('mario_background4.png')
 
     def draw(self):
         self.image.clip_draw(0 + x, 0, 800, 600, 400, 300)
@@ -12,44 +12,59 @@ class BackGround:
 
 class Character:                # 마리오
     def __init__(self):
-        self.image_left = load_image('mario_org_run_left.png')
-        self.image_right = load_image('mario_org_run_right.png')
-        self.image_jump_left = load_image('mario_org_jump_left.png')
-        self.image_jump_right = load_image('mario_org_jump_right.png')
+        self.image_org = load_image('mario_character.png')
+        self.image_left = load_image('mario_left.png')
+        self.image_right = load_image('mario_right.png')
+        self.image_jump_left = load_image('mario_jump_left.png')
+        self.image_jump_right = load_image('mario_jump_right.png')
         self.frame = 0
         self.frame_jump = 0
+        self.frame_run = 0
 
     def update(self):
-        self.frame = (self.frame + 1) % 2               # 마리오 RUN 이미지
+        self.frame = (self.frame + 1) % 9               # 마리오 스탠드 이미지
         self.frame_jump = (self.frame_jump + 1) % 5     # 마리오 Jump 이미지
+        self.frame_run = (self.frame_run + 1) % 2     # 마리오 Run 이미지
 
     def draw(self):         # 보는 방향에 대해 다른 sprite 적용
-        if jump == 1 or jump == 2:
+        if dir == 0 and cdir == 0 and jump == 0:
+            self.image_org.clip_draw(self.frame*46, 0, 46, 60, cx, cy)
+        elif jump == 1 or jump == 2:
             if dir == 1:
-                self.image_jump_right.clip_draw(220 - self.frame_jump*56, 0, 56, 60, cx, cy)
+                self.image_jump_right.clip_draw(184 - self.frame_jump*46, 0, 46, 60, cx, cy)
             elif dir == -1:
-                self.image_jump_left.clip_draw(self.frame_jump*56, 0, 56, 60, cx, cy)
+                self.image_jump_left.clip_draw(self.frame_jump*46, 0, 46, 60, cx, cy)
             else:
-                self.image_jump_right.clip_draw(220 - self.frame_jump*56, 0, 56, 60, cx, cy)
+                self.image_jump_right.clip_draw(184 - self.frame_jump*46, 0, 46, 60, cx, cy)
         elif dir == -1 or cdir == -1:
-            self.image_left.clip_draw(self.frame*56, 0, 56, 60, cx, cy)
+            self.image_left.clip_draw(self.frame_run*46, 0, 46, 60, cx, cy)
         elif dir == 1 or cdir == 1:
-            self.image_right.clip_draw(self.frame*56, 0, 56, 60, cx, cy)
-        elif dir == 0 or cdir == 0:
-            self.image_right.clip_draw(self.frame*56, 0, 56, 60, cx, cy)
+            self.image_right.clip_draw(self.frame_run*46, 0, 46, 60, cx, cy)
 
 class Object:
     def __init__(self):
-        self.image_brick1 = load_image('mario_tile1.png')
+        self.image_brick1 = load_image('mario_tile4.png')
         self.image_brick2 = load_image('mario_tile2.png')
         self.image_brick3 = load_image('mario_tile3.png')
+        self.image_coin = load_image('coin.png')
+        self.coin_frame = 0
+
+    def Coin_update(self):
+        self.coin_frame = (self.coin_frame + 1) % 24
 
     def Brick_draw1(self):
         if stage == 1:
             for i in range(0, len(brick1_1)):
-                self.image_brick1.clip_draw(0, 0, 60, 30, (60 * i) * brick1_1[i] - x, 15 - y)
-                self.image_brick1.clip_draw(0, 0, 60, 30, (60 * i) * brick1_2[i] - x, 150 - y)
-                self.image_brick1.clip_draw(0, 0, 60, 30, (60 * i) * brick1_3[i] - x, 300 - y)
+                self.image_brick1.clip_draw(0, 0, 54, 30, (54 * i) * brick1_1[i] - x, 15 - y)
+                self.image_brick1.clip_draw(0, 0, 54, 30, (54 * i) * brick1_2[i] - x, 150 - y)
+                self.image_brick1.clip_draw(0, 0, 54, 30, (54 * i) * brick1_3[i] - x, 300 - y)
+
+    def Coin_draw1(self):
+        if stage == 1:
+            for i in range(1, len(coin2)):
+                self.image_coin.clip_draw(self.coin_frame*35, 0, 35, 45, (54 * i) * coin2[i] - x, 200-y)
+                self.image_coin.clip_draw(self.coin_frame*35, 0, 35, 45, (54 * i) * coin3[i] - x, 350-y)
+
 
 
 def handle_events():
@@ -77,7 +92,6 @@ def handle_events():
                         cy_org = cy
                         cy_dir = 5
 
-
         elif event.type == SDL_KEYUP:
             if event.key == SDLK_LEFT:
                 dir = 0
@@ -92,15 +106,25 @@ open_canvas(800, 600)
 backGround = BackGround()
 Mario = Character()
 object = Object()
-brick1_1 = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1,
-          1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1,
-          1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0,
-          1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+brick1_1 = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+          1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 
 brick1_2 = [0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1,
-          0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0,
+          0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0,
           0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1,
           1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+coin2 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1,
+          0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0,
+          0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1,
+          1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+coin3 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0,
+          0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0,
+          0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1,
+          0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 brick1_3 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0,
           0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0,
@@ -137,17 +161,15 @@ while running:
     # 발판에 발을 밝고 있을 때 state
     if stage == 1:
         for i in range(0, len(brick1_1)):
-            if cx >= (60 * i * brick1_1[i] - x) - 30 and cx <= (60 * i * brick1_1[i] - x + 60) - 30 and cy >= 30 and cy <= 50:
+            if cx >= (54 * i * brick1_1[i] - x) - 30 and cx <= (54 * i * brick1_1[i] - x + 54) - 30 and cy >= 30 and cy <= 50:
                 state = 1
                 cy = 50
-            elif cx >= (60 * i * brick1_2[i] - x - 30) and cx <= (60 * i * brick1_2[i] - x + 60 - 30) and cy >= 160 and cy <= 200:
+            if cx >= (54 * i * brick1_2[i] - x - 30) and cx <= (54 * i * brick1_2[i] - x + 54 - 30) and cy >= 140 and cy <= 190:
                 state = 1
                 cy = 190
-            elif cx >= (60 * i * brick1_3[i] - x - 30) and cx <= (60 * i * brick1_3[i] - x + 60 - 30) and cy >= 310 and cy <= 350:
+            if cx >= (54 * i * brick1_3[i] - x - 30) and cx <= (54 * i * brick1_3[i] - x + 54 - 30) and cy >= 290 and cy <= 340:
                 state = 1
                 cy =340
-        if state != 1:
-            cy -= 10
 
         if dir == 0:
             cx += 5 * cdir
@@ -158,24 +180,29 @@ while running:
             cy += 50 - cy_dir
             cy_dir += 5
             for i in range(0, len(brick1_1)):
-                if cx >= (60 * i * brick1_1[i] - x ) and cx <= ( 60 * i * brick1_1[i] - x + 60 ) and cy <= 30:
+                if cx >= (54 * i * brick1_1[i] - x ) and cx <= ( 54 * i * brick1_1[i] - x + 54 ) and cy <= 50:
                     cy = 50
                     jump = 0
                     jump_count = 0
-                elif cx >= (60 * i * brick1_2[i] - x - 30) and cx <= (60 * i * brick1_2[i] - x + 60 - 30) and cy >= 160 and cy <= 200:
+                if cx >= (54 * i * brick1_2[i] - x - 30) and cx <= (54 * i * brick1_2[i] - x + 54 - 30) and cy >= 160 and cy <= 200:
                     cy = 190
                     jump = 0
                     jump_count = 0
-                elif cx >= (60 * i * brick1_3[i] - x - 30) and cx <= (60 * i * brick1_3[i] - x + 60 - 30) and cy >= 310 and cy <= 360:
+                if cx >= (54 * i * brick1_3[i] - x - 30) and cx <= (54 * i * brick1_3[i] - x + 54 - 30) and cy >= 310 and cy <= 360:
                     cy = 340
                     jump = 0
                     jump_count = 0
 
+    if state != 1:
+        jump = 1
+        if cy - 10 >= 20:
+            cy -= 10
+
         # 추락 -> 체력 게이지 깎이게 구현해야 함
-        if cy <= 0:
+        if cy <= -10:
             cy = 50
             cx = 400
-            jump = 9
+            jump = 0
             jump_count = 0
             x = 5
             y = 0
@@ -183,12 +210,14 @@ while running:
     # if dir != 0 or cdir != 0:
     #     Mario.update()
     Mario.update()
+    object.Coin_update()
 
     # game draw
     clear_canvas()
     backGround.draw()
     if stage == 1:
         object.Brick_draw1()
+        object.Coin_draw1()
     Mario.draw()
     update_canvas()
 
